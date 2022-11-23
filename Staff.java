13484f4304bc7df9d87ea2461db168ca5e0b1636
SeanONeill108 <113437214+SeanONeill108@users.noreply.github.com>
2022-11-23 16:56:33 +0000

    Add files via upload

---
+++ Staff.java
@@ -0,0 +1,57 @@
+public class Staff extends Person{
+    private String staffID;
+    /*private static int count = 1;*/
+    private double salary;
+    private String password;
+    
+    public Staff(){
+        
+    }
+    
+    public Staff(String name, String phoneNumber, String staffID,/* int count,*/ double salary, String password){
+        super(name, phoneNumber);
+        this.staffID = staffID;
+        /*this.staffID = count;
+        this.count = count++;*/
+        this.salary = salary;
+        this.password = password;
+    }
+    
+    public String getStaffID(){
+        return staffID;
+    }
+    
+    public void setStaffID(){
+        this.staffID = staffID;
+    }
+    
+    public double getSalary(){
+        return salary;
+    }
+    
+    public void setSalary(){
+        this.salary = salary;
+    }
+    
+    public String getPassword(){
+        return password;
+    }
+    
+    public void setPassword(){
+        this.password = password;
+    }
+    
+    public boolean checkPassword(String staffID, String password){
+        if(this.staffID.equals(staffID) && this.password.equals(password)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    
+    @Override
+    public String toString(){
+        return super.toString() + "\n Staff ID: " + getStaffID() + "\n Salary: " + getSalary() + "\n Password: " + getPassword();
+    }
+}
