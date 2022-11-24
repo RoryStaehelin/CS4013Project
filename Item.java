ebb34b02d1711917d85823fc7f7fa31df0e3ebff
killianC2323 <103889887+killianC2323@users.noreply.github.com>
2022-11-24 10:41:31 +0000

    Update Item.java

--- Item.java
+++ Item.java
@@ -1,21 +1,27 @@
+/**
+ * Item class is used to create new items for the restaurants
+ * @author killian
+ */
 public class Item {
+//Private data fields    
     private String name;
     private String category;
-
     private String description;
     private double price;
-
+    
+//Item constructor
     public Item(String name, String category, String description, double price) {
         this.name = name;
         this.category = category;
         this.description = description;
         this.price = price;
     }
-
+    
+//Accessor and Mutator methods
     public String getName() {
         return name;
     }
-
+    
     public void setName(String name) {
         this.name = name;
     }
@@ -43,7 +49,7 @@ public class Item {
     public void setPrice(double price) {
         this.price = price;
     }
-
+//to string method to convert item object into a string
     @Override
     public String toString() {
         return String.format("Name: %s\nDescription: %s\nCategory: %s\nPrice: %,.2f", name, description, category, price);
