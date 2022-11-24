8f943b8cdadf3984e77f06b44019c638a748c6e1
hongtao <91566677+william09282021@users.noreply.github.com>
2022-11-24 04:25:12 +0000

    Update Statistics.java

--- Statistics.java
+++ Statistics.java
@@ -1,16 +1,56 @@
 import java.time.LocalDate;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList; 
 
 public class Statistics {
     private String statistic;
+    
     Statistics()
     {
-        //Loads data from statistics.csv for all dates and saves them in a nice formatted way in statistic
+        String line = "";
+        try{
+            BufferedReader br = new BufferedReader(new FileReader("statistics.csv"));
+            while((line = br.readLine()) != null){
+                String[] values = line.split(",");
+                System.out.println("Date : " + values[0] + ", "
+                                 + "Amount: " + values[1]);             
+            }
+            
+        } catch(FileNotFoundException e){
+            e.printStackTrace();            
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+        
     }
+    
     Statistics(LocalDate startDate, LocalDate endDate)
     {
-        //Loads data from statistics.csv between specified dates and saves them in a nice formatted way in statistic
+        ArrayList<String> data = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader("statistics.csv"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+                LocalDate sd = LocalDate.parse(values[0]);
+                LocalDate ed = LocalDate.parse(values[0]);
+                if(startDate.isAfter(sd) && endDate.isBefore(ed)){
+                    data.add(values[0] + ", " + values[1]);
+                }
+                System.out.println(data);
+                
+            }
+            
+        } catch(FileNotFoundException e){
+            e.printStackTrace();            
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+        
     }
-
+    
     public String getStatistic()
     {
         return statistic;
