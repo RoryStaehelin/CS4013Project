1e8c1e87e9cda2f6f7878024baf109b25c08979f
hongtao <91566677+william09282021@users.noreply.github.com>
2022-11-24 02:16:47 +0000

    Update Statistics.java

--- Statistics.java
+++ Statistics.java
@@ -4,7 +4,26 @@ public class Statistics {
     private String statistic;
     Statistics()
     {
-        //Loads data from statistics.csv for all dates and saves them in a nice formatted way in statistic
+        String fileName = "Resturant.csv";
+        String line = "";
+        try{
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+            
+            while((line = br.readLine()) != null){
+                String[] values = line.split(",");
+                System.out.println("ReservationId : " + values[0] + ", "
+                                 + "numPeople: " + values[1] + ", "
+                                 + "date: " + values[2] + ", "
+                                 + "time : " + values[3] + ", "
+                                 + "tableNo: " + values[4] + ", "
+                                 + "customerId: " + values[5]);              
+            }
+            
+        } catch(FileNotFoundException e){
+            e.printStackTrace();            
+        } catch(IOException e){
+            e.printStackTrace();
+        }
     }
     Statistics(LocalDate startDate, LocalDate endDate)
     {
